6991524828f3fc6474c2a01ba64072550e3e9ddd
Gus <guusflorez@gmail.com>
2018-06-11 15:51:33 +0200

    added search-feature

---
+++ src/Components/PhotoList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Photo from './Photo';
+import NoPhotos from './NoPhotos';
+
+const PhotoList = props => {
+
+
+  return (
+    <div className="photo-container">
+      <h2>Results</h2>
+      <ul>
+        
+      </ul>
+    </div>
+  );
+}
+
+export default PhotoList;
